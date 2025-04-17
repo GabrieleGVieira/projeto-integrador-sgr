39af7bff557b19b1f1a6ed08c724a65c8f06ce58
Gabriele Gonçalves Vieira <gabrielevieira011@gmail.com>
2025-04-17 15:09:52 -0300

    feat: create new endpoints to product

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -15,3 +15,79 @@ export async function GET() {
     );
   }
 }
+
+export async function PATCH(req) {
+  try {
+    const { id, price, category, active } = await req.json();
+
+    const updated = await db.product.update({
+      where: { id },
+      data: {
+        price,
+        category,
+        active,
+        updatedAt: new Date(),
+      },
+    });
+
+    return new Response(JSON.stringify(updated), { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function DELETE(req) {
+  try {
+    const { id } = await req.json();
+
+    await db.product.delete({
+      where: { id },
+    });
+
+    return new Response(JSON.stringify({ success: true }), { status: 200 });
+  } catch (error) {
+    console.log(error)
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+    });
+  }
+}
+
+export async function POST(req) {
+  try {
+    const { name, price, category, active } = await req.json();
+
+    if (!name || !category || price == null) {
+      return new Response(
+        JSON.stringify({
+          error: "Nome, categoria e preço são obrigatórios",
+        }),
+        { status: 400 }
+      );
+    }
+
+    const newProduct = await db.product.create({
+      data: {
+        name,
+        price,
+        category,
+        active,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    });
+
+    return new Response(JSON.stringify(newProduct), { status: 201 });
+  } catch (error) {
+    console.error("Erro ao criar produto:", error);
+    return new Response(
+      JSON.stringify({
+        error: error.message || "Erro desconhecido",
+        stack: error.stack || "Sem detalhes da stack",
+      }),
+      { status: 500 }
+    );
+  }
+}
